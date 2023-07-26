75686feb1ef4615fa8329f232bc33af8dfb63fe9
badx86 <badlolpro@gmail.com>
2023-07-27 02:00:13 +0400

    add SQLAlchemy models: ItemModel & StoreModel

---
+++ models/store.py
@@ -0,0 +1,13 @@
+from db import db
+
+
+class StoreModel(db.Model):
+    """
+    Модель представляет таблицу 'stores' в базе данных
+    Связывает объект StoreModel с таблицей 'stores' в базе данных и определяет его структуру
+    """
+    __tablename__ = "stores"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), unique=True, nullable=False)
+    items = db.relationship("ItemModel", back_populates="store", lazy="dynamic")
