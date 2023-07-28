5cd86652cb45a0405b022a87bab2a674c1574700
badx86 <badlolpro@gmail.com>
2023-07-28 15:32:41 +0400

    add JWT & the User model and schema

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 import os
+import secrets
 from db import db
 from flask import Flask
 from flask_smorest import Api
+from flask_jwt_extended import JWTManager
 from resources.item import blp as ItemBlueprint
 from resources.store import blp as StoreBlueprint
 from resources.tag import blp as TagBlueprint
+from resources.user import blp as UserBlueprint
 
 
 def create_app(db_url=None):
@@ -31,7 +34,11 @@ def create_app(db_url=None):
     )
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.init_app(app)
+
     api = Api(app)
+    # 2 get your secret key: terminal -> python -> import secrets -> secrets.SystemRandom().getrandbits(128)
+    app.config["JWT_SECRET_KEY"] = "test"
+    jwt = JWTManager(app)
 
     @app.before_request
     def create_tables():
@@ -46,5 +53,6 @@ def create_app(db_url=None):
     api.register_blueprint(ItemBlueprint)
     api.register_blueprint(StoreBlueprint)
     api.register_blueprint(TagBlueprint)
+    api.register_blueprint(UserBlueprint)
 
     return app
